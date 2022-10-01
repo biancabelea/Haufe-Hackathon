341c18a1ae04d8657a2f028063c0124a9e162a90
Bianca Belea <bianca.belea@student.upt.ro>
2022-10-01 15:16:46 +0300

    Show popup on map with hardcoded coordinations

--- hack-app/src/components/Map.js
+++ hack-app/src/components/Map.js
@@ -5,7 +5,11 @@ import { Icon } from 'leaflet';
 import './Map.css';
 
 const Haufe = [45.75951, 21.220871];
-const Punct1 = [45.777078, 21.220764]
+const Punct1 = [45.777078, 21.220764];
+const Punct2 = [45.775032, 21.223086];
+const Punct3 = [45.745192, 21.242933];
+const Punct4 = [45.771387, 21.221789];
+const Punct5 = [45.702321, 21.212351];
 
 function Map() {
   return (
@@ -38,6 +42,50 @@ function Map() {
         >
         <Popup>
           Cugir, Aradului
+        </Popup>
+      </Marker>
+      <Marker
+          position={Punct2}
+          icon={new Icon({
+            iconUrl: "/pin.svg",
+            iconSize: [25, 25]
+          })}
+        >
+        <Popup>
+          Linistei, Rasaritului
+        </Popup>
+      </Marker>
+      <Marker
+          position={Punct3}
+          icon={new Icon({
+            iconUrl: "/pin.svg",
+            iconSize: [25, 25]
+          })}
+        >
+        <Popup>
+          Carturarilor, Intrarea Padurarilor
+        </Popup>
+      </Marker>
+      <Marker
+          position={Punct4}
+          icon={new Icon({
+            iconUrl: "/pin.svg",
+            iconSize: [25, 25]
+          })}
+        >
+        <Popup>
+          Calan, Carei
+        </Popup>
+      </Marker>
+      <Marker
+          position={Punct5}
+          icon={new Icon({
+            iconUrl: "/pin.svg",
+            iconSize: [25, 25]
+          })}
+        >
+        <Popup>
+          Primaverii, Nicolae Firu
         </Popup>
       </Marker>
     </MapContainer>
